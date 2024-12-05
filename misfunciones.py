7de9eba8a77dcb745bdc37931f4a4e783755c62d
Rodolfo Barbeito <rbarbeito@gmail.com>
2024-12-05 11:11:14 +0100

    Segundo dia

--- misfunciones.py
+++ misfunciones.py
@@ -1,13 +1,11 @@
+from datetime import datetime, timedelta, time
+from urllib.parse import unquote
 
-def cabecera(contentlength, cookie=False, archivo='html'):
-    
-    cabecera = '''HTTP/1.1 200 OK
-                        Server: Apache
-                        Content-Length: {}
-                        Content-Type: {}; chartset=utf-8
-                        Set-Cookie: id=a3fWa; Max-Age=2592000\r\n\r\n'''.format(contentlength, tipos_mime(archivo))
-
+def cabecera(contentlength, cookie, archivo):
+    cabecera = 'HTTP/1.1 200 OK\r\nServer: Apache\r\nContent-Length: {}\r\nContent-Type: {}; chartset=utf-8\r\n{}\r\n\r\n'''.format(contentlength, tipos_mime(archivo),addcookie(cookie))
     return cabecera
+
+
 
 def tipos_mime(archivo):
     
@@ -19,9 +17,25 @@ def tipos_mime(archivo):
     
     return tiposmime[archivo]
 
+
+
 def addcookie(cookie):
-    print(cookie)
+
     if cookie == True:
-        return "Set-Cookie: id=a3fWa; Max-Age=2592000"
+        return "Set-Cookie: id=a3fWa; Expires={};".format(tiempo_cookie())
     
     return ''
+
+
+
+def tiempo_cookie():
+    dt = datetime.now() + timedelta(minutes=10)
+
+    return dt.strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+def convertir_ascii(texto):
+    return unquote(texto).replace('+', " ")
+
+
+
+    
--- server.py
+++ server.py
@@ -17,11 +17,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         
         conn, addr= s.accept()
-        
+
         with conn: 
             print("{}> Cliente conectado desde... {}".format(pid,addr))
             
             data = conn.recv(1024)
+
+            print(data.decode().split('\r\n'))
+
             
             metodo, ruta, protocolo = data.decode('utf-8').split('\r\n')[0].split(' ')
             
@@ -31,7 +34,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     with open('login.html', 'r', encoding='utf-8') as f:
                         contenido = f.read()
                         
-                    contenido = mf.cabecera(len(contenido), True) + contenido
+                    contenido = mf.cabecera(len(contenido), False, 'html') + contenido
                     conn.sendall(contenido.encode('utf-8'))
                 
                 elif metodo == "POST":
@@ -40,26 +43,53 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 else:
                     pass  
                 
-            elif ruta == 'login':
+            elif ruta == '/login':
                 
                 if metodo =='GET':
                     pass 
                 
                 elif metodo =='POST':
-                    pass
-                
+                    campos = data.decode('utf-8').split('\r\n')[-1].split('&')
+                     
+                    email = mf.convertir_ascii(campos[0].split('=')[-1])
+                    passwd = mf.convertir_ascii(campos[1].split('=')[-1])
+
+                    datos_user=dict()
+                     
+                    for campo in campos:
+                        forma = mf.convertir_ascii(campo).split('=')
+                        datos_user[forma[0]]=forma[1]
+
+                    if datos_user["email"]=='xx@xx.com':
+
+                        if datos_user['password'] == '123456':
+                            content="<html ><head><meta charset='UTF-8'></head><body><h1>OK, makey</h1></body></html>"
+                            contenido = mf.cabecera(len(content),True,'html') + content
+                            conn.sendall(contenido.encode('utf-8'))
+                        else:
+                            content="<html ><head><meta charset='UTF-8'></head><body><h1>Error de usuario y/o contraseña</h1></body></html>"
+                            contenido = mf.cabecera(len(content),False,'html') + content
+                            conn.sendall(contenido.encode('utf-8'))
+                    else:
+                            content="<html ><head><meta charset='UTF-8'></head><body><h1>Error de usuario y/o contraseña</h1></body></html>"
+                            contenido = mf.cabecera(len(content),False,'html') + content
+                            conn.sendall(contenido.encode('utf-8'))
+                            
+
+                    
                 else:
                     pass  
                    
             else:
                 
                 extension = ruta.split('/')[-1].split('.')[-1]
-                
+
+                                
                 if extension in ['css','js']:
                     with open('.'+ruta,'r') as f:
                         archivo = f.read()
                         
-                    archivo = mf.cabecera(len(archivo),extension) + archivo
+                    archivo = mf.cabecera(len(archivo), False ,extension) + archivo
                     conn.sendall(archivo.encode('utf-8'))  
                 
         
